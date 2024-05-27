a8f4a3748ba9e20d559b99ff8343b0ad9ae6c1bd
gabrielLAraujo <bfrauches@gmail.com>
2024-05-27 20:41:47 -0300

    criacao do user controller

--- GYMLogAPI/Entities/User.cs
+++ GYMLogAPI/Entities/User.cs
@@ -1,4 +1,6 @@
-﻿namespace GYMLogAPI.Entities
+﻿using GYMLogAPI.Entities.DTOs;
+
+namespace GYMLogAPI.Entities
 {
     public class User
     {
@@ -6,5 +8,15 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public User()
+        {
+
+        }
+        public User(UserDTO dto) {
+            token = new Guid().ToString();
+            Name = dto.Name;
+            Email = dto.Email;
+            Password = dto.Password;
+        }
     }
 }
